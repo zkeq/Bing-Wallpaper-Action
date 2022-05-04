23eecb8c5a1969612ba56b4a57e1ee4813c13068
Zkeq <learnonly@learnonly.xyz>
2022-05-04 23:41:22 +0800

    vercel

--- api/index.py
+++ api/index.py
@@ -15,8 +15,8 @@ r = redis.Redis(
 
 def get_bing():
     _params_data = r.srandmember("bing_images", -1)[0].decode('utf-8')
-    full_uel = "https://bing.com" + _params_data
-    return _params_data
+    full_uel = "https://bing.com/" + _params_data
+    return full_uel
 
 
 class handler(BaseHTTPRequestHandler):
@@ -26,7 +26,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('location', params_data)  # 这个是主要的
         self.send_header('Refresh', '0;url={}'.format(params_data))
-        self.send_header('Cache-Control', 'max-age=0, s-maxage=60, stale-while-revalidate=3600')  # vercel 缓存
+        # self.send_header('Cache-Control', 'max-age=0, s-maxage=60, stale-while-revalidate=3600')  # vercel 缓存
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write('Redirecting to {} (308)'.format(params_data).encode('utf-8'))  # 这里无所谓
