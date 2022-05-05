9fa71f8d1db5e735d04c33ac602269fbfb11e484
Zkeq <learnonly@learnonly.xyz>
2022-05-05 19:01:01 +0800

    ALL IN ONE

--- en-US_post_to_redis.py
+++
@@ -1,6 +0,0 @@
-# coding:utf-8
-
-import post_to_redis
-
-if __name__ == '__main__':
-    post_to_redis.main("en-US")
--- zh-CN_main.py
+++
@@ -1,5 +0,0 @@
-# coding:utf-8
-import main
-
-if __name__ == '__main__':
-    main.main("zh-CN")
