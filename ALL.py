5d2eca966ccf2a37d782db6345015ed97795b486
Zkeq <learnonly@learnonly.xyz>
2022-05-05 20:31:08 +0800

    去除冗余数据

--- ALL.py
+++ ALL.py
@@ -6,27 +6,7 @@ import main
 import post_to_redis
 
 work_list = [
-    "ar-XA", "bg-BG", "cs-CZ", "da-DK", "de-AT", "de-CH", "de-DE", "el-GR", "en-AU", "en-CA", "en-GB", "en-ID",
-    "en-IE", "en-IN", "en-MY", "en-NZ", "en-PH", "en-SG", "en-US", "en-XA", "en-ZA", "es-AR", "es-CL", "es-ES",
-    "es-MX", "es-US", "es-XL", "et-EE", "fi-FI", "fr-BE", "fr-CA", "fr-CH", "fr-FR", "he-IL", "hr-HR", "hu-HU",
-    "it-IT", "ja-JP", "ko-KR", "lt-LT", "lv-LV", "nb-NO", "nl-BE", "nl-NL", "pl-PL", "pt-BR", "pt-PT", "ro-RO",
-    "ru-RU", "sk-SK", "sl-SI", "sv-SE", "th-TH", "tr-TR", "uk-UA", "zh-CN", "zh-HK", "zh-TW"
-]
-
-lang_list = [
-    "Arabic – Arabia", "Bulgarian – Bulgaria", "Czech – Czech Republic", "Danish – Denmark", "German – Austria",
-    "German – Switzerland", "German – Germany", "Greek – Greece", "English – Australia", "English – Canada",
-    "English - United Kingdom", "English – Indonesia", "English – Ireland", "English – India", "English – Malaysia",
-    "English – New Zealand", "English – Philippines", "English – Singapore", "English – United States",
-    "English – Arabia", "English – South Africa", "Spanish – Argentina", "Spanish – Chile",
-    "Spanish – Spain", "Spanish – Mexico", "Spanish – United States", "Spanish – Latin America",
-    "Estonian – Estonia", "Finnish – Finland", "French – Belgium", "French – Canada", "French – Switzerland",
-    "French – France", "Hebrew – Israel", "Croatian – Croatia", "Hungarian – Hungary", "Italian – Italy",
-    "Japanese – Japan", "Korean – Korea", "Lithuanian – Lithuania", "Latvian – Latvia", "Norwegian – Norway",
-    "Dutch – Belgium", "Dutch – Netherlands", "Polish – Poland", "Portuguese – Brazil", "Portuguese – Portugal",
-    "Romanian – Romania", "Russian – Russia", "Slovak – Slovak Republic", "Slovenian – Slovenia", "Swedish – Sweden",
-    "Thai – Thailand", "Turkish – Turkey", "Ukrainian – Ukraine", "Chinese – China", "Chinese – Hong Kong SAR",
-    "Chinese – Taiwan"
+    "de-DE", "en-CA", "en-GB", "en-IN", "en-US", "fr-FR", "it-IT", "ja-JP", "zh-CN"
 ]
 
 
@@ -68,5 +48,5 @@ for i in work_list:
         os.mkdir(f"data/{i}_daily_log")
     main.main(i)
     post_to_redis.main(i)
-    time.sleep(10)
+    time.sleep(3)
 
--- make_readme.py
+++ make_readme.py
@@ -7,6 +7,7 @@ def get_now_time():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 
+#   "zh-CN", "en-US", "ja-JP", "de-DE", "en-CA", "en-GB", "en-IN",  "fr-FR", "it-IT"
 # 读取 data/zh-CN_all.json 文件，生成 README.md 文件
 with open('data/zh-CN_all.json', 'r', encoding='utf-8') as f:
     zh_data = json.load(f)
@@ -16,6 +17,25 @@ with open('data/en-US_all.json', 'r', encoding='utf-8') as f:
 
 with open('data/ja-JP_all.json', 'r', encoding='utf-8') as f:
     ja_data = json.load(f)
+
+with open('data/de-DE_all.json', 'r', encoding='utf-8') as f:
+    de_data = json.load(f)
+
+with open('data/en-CA_all.json', 'r', encoding='utf-8') as f:
+    en_ca_data = json.load(f)
+
+with open('data/en-GB_all.json', 'r', encoding='utf-8') as f:
+    en_gb_data = json.load(f)
+
+with open('data/en-IN_all.json', 'r', encoding='utf-8') as f:
+    en_in_data = json.load(f)
+
+with open('data/fr-FR_all.json', 'r', encoding='utf-8') as f:
+    fr_data = json.load(f)
+
+with open('data/it-IT_all.json', 'r', encoding='utf-8') as f:
+    it_data = json.load(f)
+
 
 all_day = len(zh_data['data'])
 print("[{}] all day: {}".format(get_now_time(), all_day))
@@ -29,7 +49,7 @@ f.write("# Bing Wallpaper\n")
 f.write(f"<!--{get_now_time()}-->\n")
 f.write("![{0}]({2}) Today: [{0}]({1})\n".format(head_title, head_img, head_img + "&w=1000"))
 f.write("""
-|  zh-CN   |   en-US   |   ja-JP   |
+|  Chinese – China   |   English – United Kingdom   |   Japanese – Japan   |
 | :----: | :----: | :----: |
 """)
 for i in range(all_day):
@@ -50,4 +70,52 @@ for i in range(all_day):
     ja_url_full = "https://www.bing.com" + ja_day['urlbase'] + "_UHD.jpg"
     ja_readme_url = ja_url_full + "&pid=hp&w=384&h=216&rs=1&c=4"
     f.write("| ![{0}]({1}) {0} [download 4k]({6})| ![{2}]({3}) {2} [download 4k]({7})| ![{4}]({5}) {4} [download 4k]({8})|\n".format(zh_date_format, zh_readme_url, en_date_format, en_readme_url, ja_date_format, ja_readme_url, zh_url_full, en_url_full, ja_url_full))
-f.close()
+
+
+f.write("""
+|  German – Germany   |   English – Canada   |   English – United States   |
+| :----: | :----: | :----: |
+""")
+for i in range(all_day):
+    print("[{}] day: {}".format(get_now_time(), i + 1))
+    de_day = de_data['data'][i]
+    en_ca_day = en_ca_data['data'][i]
+    en_gb_day = en_gb_data['data'][i]
+    de_date = de_day['startdate']
+    de_date_format = "{}-{}-{}".format(de_date[0:4], de_date[4:6], de_date[6:8])
+    en_ca_date = en_ca_day['startdate']
+    en_ca_date_format = "{}-{}-{}".format(en_ca_date[0:4], en_ca_date[4:6], en_ca_date[6:8])
+    en_gb_date = en_gb_day['startdate']
+    en_gb_date_format = "{}-{}-{}".format(en_gb_date[0:4], en_gb_date[4:6], en_gb_date[6:8])
+    de_url_full = "https://www.bing.com" + de_day['urlbase'] + "_UHD.jpg"
+    de_readme_url = de_url_full + "&pid=hp&w=384&h=216&rs=1&c=4"
+    en_ca_url_full = "https://www.bing.com" + en_ca_day['urlbase'] + "_UHD.jpg"
+    en_ca_readme_url = en_ca_url_full + "&pid=hp&w=384&h=216&rs=1&c=4"
+    en_gb_url_full = "https://www.bing.com" + en_gb_day['urlbase'] + "_UHD.jpg"
+    en_gb_readme_url = en_gb_url_full + "&pid=hp&w=384&h=216&rs=1&c=4"
+    f.write("| ![{0}]({1}) {0} [download 4k]({6})| ![{2}]({3}) {2} [download 4k]({7})| ![{4}]({5}) {4} [download 4k]({8})|\n".format(de_date_format, de_readme_url, en_ca_date_format, en_ca_readme_url, en_gb_date_format, en_gb_readme_url, de_url_full, en_ca_url_full, en_gb_url_full))
+
+
+f.write("""
+|  English – India  |   French – France   |   Italian – Italy   |
+| :----: | :----: | :----: |
+""")
+
+for i in range(all_day):
+    print("[{}] day: {}".format(get_now_time(), i + 1))
+    en_in_day = en_in_data['data'][i]
+    fr_fr_day = fr_data['data'][i]
+    it_it_day = it_data['data'][i]
+    en_in_date = en_in_day['startdate']
+    en_in_date_format = "{}-{}-{}".format(en_in_date[0:4], en_in_date[4:6], en_in_date[6:8])
+    fr_fr_date = fr_fr_day['startdate']
+    fr_fr_date_format = "{}-{}-{}".format(fr_fr_date[0:4], fr_fr_date[4:6], fr_fr_date[6:8])
+    it_it_date = it_it_day['startdate']
+    it_it_date_format = "{}-{}-{}".format(it_it_date[0:4], it_it_date[4:6], it_it_date[6:8])
+    en_in_url_full = "https://www.bing.com" + en_in_day['urlbase'] + "_UHD.jpg"
+    en_in_readme_url = en_in_url_full + "&pid=hp&w=384&h=216&rs=1&c=4"
+    fr_fr_url_full = "https://www.bing.com" + fr_fr_day['urlbase'] + "_UHD.jpg"
+    fr_fr_readme_url = fr_fr_url_full + "&pid=hp&w=384&h=216&rs=1&c=4"
+    it_it_url_full = "https://www.bing.com" + it_it_day['urlbase'] + "_UHD.jpg"
+    it_it_readme_url = it_it_url_full + "&pid=hp&w=384&h=216&rs=1&c=4"
+    f.write("| ![{0}]({1}) {0} [download 4k]({6})| ![{2}]({3}) {2} [download 4k]({7})| ![{4}]({5}) {4} [download 4k]({8})|\n".format(en_in_date_format, en_in_readme_url, fr_fr_date_format, fr_fr_readme_url, it_it_date_format, it_it_readme_url, en_in_url_full, fr_fr_url_full, it_it_url_full))
