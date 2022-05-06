dd7017a5ff541eff5663ae080f8938dbe71f6089
zkeq <admin@icodeq.com>
2022-05-06 23:09:31 +0800

    分类爬取，使日志更优雅

--- ALL.py
+++ ALL.py
@@ -4,10 +4,12 @@ import time
 
 import main
 import post_to_redis
+import sys
 
-work_list = [
-    "de-DE", "en-CA", "en-GB", "en-IN", "en-US", "fr-FR", "it-IT", "ja-JP", "zh-CN"
-]
+
+# work_list = [
+#     "de-DE", "en-CA", "en-GB", "en-IN", "en-US", "fr-FR", "it-IT", "ja-JP", "zh-CN"
+# ]
 
 
 # 判断文件是否存在
@@ -29,24 +31,25 @@ def get_all_template():
 
 def get_update_template():
     # 读取 data/template_update.json 文件
-    with open("data/template_update.json", "r") as f:
-        template_update = f.read()
+    with open("data/template_update.json", "r") as _f:
+        template_update = _f.read()
     return template_update
 
 
-for i in work_list:
-    if not file_exists(f"data/{i}_all.json"):
-        # 创建文件
-        with open(f"data/{i}_all.json", "w", encoding="utf-8") as f:
-            f.write(get_all_template())
-    if not file_exists(f"data/{i}_update.json"):
-        # 创建文件
-        with open(f"data/{i}_update.json", "w", encoding="utf-8") as f:
-            f.write(get_update_template())
-    if not dir_exists(f"data/{i}_daily_log"):
-        # 创建文件夹
-        os.mkdir(f"data/{i}_daily_log")
-    main.main(i)
-    post_to_redis.main(i)
-    time.sleep(3)
+i = sys.argv[1]
+
+if not file_exists(f"data/{i}_all.json"):
+    # 创建文件
+    with open(f"data/{i}_all.json", "w", encoding="utf-8") as f:
+        f.write(get_all_template())
+if not file_exists(f"data/{i}_update.json"):
+    # 创建文件
+    with open(f"data/{i}_update.json", "w", encoding="utf-8") as f:
+        f.write(get_update_template())
+if not dir_exists(f"data/{i}_daily_log"):
+    # 创建文件夹
+    os.mkdir(f"data/{i}_daily_log")
+main.main(i)
+post_to_redis.main(i)
+time.sleep(3)
 
